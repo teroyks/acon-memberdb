3da57620e590d59ce630c9d1fd7a671f41d21933
Tero Ykspetäjä <tero.ykspetaja@codeworks.fi>
2019-02-14 18:23:07 +0200

    Move Route wrappers to helper module

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react'
-import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, NavLink } from 'react-router-dom'
 
 import { firebaseAuth } from '../firebase'
+import { AuthRouteWithProps } from './route-helper'
 
 enum Role {
   admin = 'Administrator',
@@ -44,53 +45,6 @@ const AuthUser = ({ name }) => {
   console.log('AuthUser', name)
   return <div>AuthUser {name}</div>
 }
-
-// Properties passed to RouteWithProps
-interface RouteProps {
-  component: any // rendered component
-  componentProps?: any // properties passed to the component
-  [propName: string]: any // route properties
-}
-// Properties passed to AuthRoute
-interface AuthRouteProps {
-  component: any // rendered component
-  auth: boolean // tells if the component should be rendered
-  [propName: string]: any // route properties
-}
-
-/**
- * Pass properties to rendered component
- * @param param0 RouteProps
- */
-const RouteWithProps = ({
-  component: Component,
-  componentProps = {},
-  ...routeProps
-}: RouteProps) => (
-  <Route {...routeProps} render={(props) => <Component {...props} {...componentProps} />} />
-)
-
-/**
- * Only render component route if authentication is successful
- * @param param0
- */
-const AuthRoute = ({ component: Component, auth, ...rest }: AuthRouteProps) => (
-  <Route {...rest} render={(props) => (auth ? <Component {...props} /> : null)} />
-)
-
-/**
- * Render component and pass properties if authentication is successful
- * @param param0
- */
-const AuthRouteWithProps = ({
-  component: Component,
-  componentProps,
-  auth,
-  ...routeProps
-}: RouteProps & AuthRouteProps) =>
-  auth ? (
-    <RouteWithProps component={Component} componentProps={componentProps} {...routeProps} />
-  ) : null
 
 const notLoggedInMessage = () => <a href="/login">Log in</a>
 
