ece4e2bbfd3523de86f6e03e85c7404d9a1000a3
Tero Ykspetäjä <tero.ykspetaja@codeworks.fi>
2019-02-15 19:31:55 +0200

    Add check for change.before and change.after
    - in case of undefined
    
    - also some non-functional reformats
    
      - new settings use longer line length

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,9 +9,7 @@ import List, * as membersList from './memberslist'
  */
 export const addTimestampToNewPurchase = functions.firestore
   .document('purchases/{purchaseId}')
-  .onCreate((snapshot) =>
-    snapshot.ref.set(createdAtTimestamp(), { merge: true })
-  )
+  .onCreate((snapshot) => snapshot.ref.set(createdAtTimestamp(), { merge: true }))
 
 /**
  * Update generated names and add 'createdAt' timestamp to new member records.
@@ -20,9 +18,7 @@ export const addNewMember = functions.firestore
   .document('members/{memberId}')
   .onCreate((snapshot) => {
     const memberData = snapshot.data() as MemberNames
-    console.log(
-      `New member added: ${memberData.firstName} ${memberData.lastName}`
-    )
+    console.log(`New member added: ${memberData.firstName} ${memberData.lastName}`)
     const memberWithUpdatedNameData = updateNameData(memberData)
 
     return snapshot.ref.set(
@@ -39,12 +35,14 @@ export const addNewMember = functions.firestore
 export const updateMember = functions.firestore
   .document('members/{memberId}')
   .onUpdate(async (change) => {
-    const oldMemberData = change.before.data() as MemberNames
-    console.log(
-      `Member update triggered: ${oldMemberData.firstName} ${
-        oldMemberData.lastName
-      }`
-    )
+    const oldMemberData = change.before
+      ? (change.before.data() as MemberNames)
+      : ({} as MemberNames)
+    console.log(`Member update triggered: ${oldMemberData.firstName} ${oldMemberData.lastName}`)
+    if (!change.after) {
+      console.log('No after data set - exiting')
+      return null
+    }
     const submittedMemberData = change.after.data() as MemberNames
 
     if (!namesHaveChanged(oldMemberData, submittedMemberData)) {
@@ -70,18 +68,11 @@ export const updateMember = functions.firestore
 export const listMembers = functions.https.onRequest(async (req, res) => {
   res.setHeader('content-type', 'text/plain')
   try {
-    const membersPromise = store.fetchMembers() as Promise<
-      membersList.memberData[]
-    >
-    const [allMembers, importedAt] = await Promise.all([
-      membersPromise,
-      store.fetchUpdateDate(),
-    ])
+    const membersPromise = store.fetchMembers() as Promise<membersList.memberData[]>
+    const [allMembers, importedAt] = await Promise.all([membersPromise, store.fetchUpdateDate()])
     const members = new List(allMembers)
 
-    const updateMessage = `Updated on ${
-      importedAt ? importedAt.toDateString() : 'Unknown'
-    }`
+    const updateMessage = `Updated on ${importedAt ? importedAt.toDateString() : 'Unknown'}`
     res.send(members.toMarkdown() + '\n\n' + updateMessage)
   } catch (err) {
     console.error('Could not fetch members list')
