4b0c30b2a453d24831ea96a2acda6120808810aa
Tero Ykspetäjä <tero.ykspetaja@codeworks.fi>
2019-02-21 18:17:43 +0200

    Add common member data interface
    - only contains stuff used so far
    
      - more to be added later

---
+++ common/member.ts
@@ -0,0 +1,10 @@
+interface Member {
+  checkDisplayNameSort: boolean
+  displayName: string
+  displayNameSort: string
+  firstName: string
+  memberId: string
+  lastName: string
+}
+
+export { Member }
